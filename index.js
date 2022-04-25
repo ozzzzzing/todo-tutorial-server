1b5b79c84b8856ed8ffa09761d9020413d513acc
0p0lll <45o245o5@gmail.com>
2022-04-25 13:53:06 +0900

    서버 첫 구동

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello world")
+var express = require("express")
+var app = express()//app: 객체
+
+app.get("/hello",(req, res)=>{
+    res.send("hello world")
+})
+//맨 밑에 놓기
+app.listen(5000, ()=>{
+    console.log("server is running on port 5000 !!!")
+})
